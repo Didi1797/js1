1c5f653ab8b7249ea53490028db9da45dab527cf
didi1797 <arturrevtovich@gmail.com>
2021-12-17 18:14:27 +0300

    add files

---
+++ homework1/index.js
@@ -0,0 +1,49 @@
+let str = 'hello';
+console.log(typeof str);
+
+let num = 123;
+console.log(typeof num);
+
+let flag = true;
+console.log(typeof flag);
+
+let txt = 'true';
+console.log(typeof txt);
+
+
+let a = 5;
+let b = 3;
+let c = 75;
+let d = 'kg';
+let i = '5';
+let f = '3';
+
+let a1 = a + b;
+let a2 = a - b;
+let a3 = a * b;
+let a4 = a / b;
+let a5 = a ** b;
+let a6 = a % b;
+let a7 = b % a;
+let a8 = a + f;
+let a9 = i - b;
+let a10 = c + d;
+console.log(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10)
+
+
+let height = 23;
+let width = 10;
+
+let s = height * width;
+console.log(s);
+
+
+let heightC = 10;
+let dC1 = 4;
+let pi = 3.1415926535;
+let con = 4;
+let con1 = 2;
+let dC = 4 ** 2
+
+let v = heightC * pi * dC / con
+console.log(v)
