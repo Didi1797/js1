0edc2e693daf48c81b3afb8e2979c946aca71a9e
didi1797 <arturrevtovich@gmail.com>
2021-12-27 00:09:58 +0300

    add 5 tasks

---
+++ homework4/index.js
@@ -0,0 +1,61 @@
+// const Sum = (a, b) => {
+//     if (a + b > 10) {
+//         console.log(true)
+//     }
+//     else {
+//         console.log(false)
+//     }
+// }
+
+// Sum(5,6)
+
+// const CheckPos = (a) => {
+//     if (a > 0) {
+//         console.log(true)
+//     }
+//     else {
+//         console.log(false)
+//     }
+// }
+
+// CheckPos(-4)
+
+// let a = 'const_text_hello';
+// const upperTH = (str) => {
+//   let result = str[0].split('_').join('') 
+//   return result;
+// };
+
+//  console.log(upperTH(a));
+
+// let fruits = ['Apple','Pen','Orange']
+
+// const inArray = (txt, arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == txt) {
+//             console.log(true)
+//         }
+//         else {
+//             console.log(false)
+//         }
+//     }
+// }
+
+// inArray('Pen', fruits)
+
+// const arrayFill = (elm, num) => {
+//     let arr = []
+//     for (let i = 0; i < num; i++) {
+//         arr.push(elm)
+        
+//     }
+//     return arr
+// }
+
+// console.log(arrayFill('x', 11))
+
+
+
+
+
+
